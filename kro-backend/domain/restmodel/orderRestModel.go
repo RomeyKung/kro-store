b64f60dc76ca1e29b49ea5abb62742cd10d618eb
kenmuraki5 <89627339+Kenmuraki5@users.noreply.github.com>
2024-03-11 23:13:27 +0700

    fix update rest model.go

--- kro-backend/domain/restmodel/orderRestModel.go
+++ kro-backend/domain/restmodel/orderRestModel.go
@@ -1,16 +1,12 @@
 package restmodel
 
-import (
-	"time"
-)
-
 type Order struct {
-	ProductId       string    `json:"productId"`
-	Quantity        int       `json:"quantity"`
-	Email           string    `json:"email"`
-	OrderDate       time.Time `json:"orderDate"`
-	Subtotal        float64   `json:"subtotal"`
-	ShippingAddress string    `json:"shippingAddress"`
-	ShippingMethod  string    `json:"shippingMethod"`
-	Type            string    `json:"type"`
+	ProductId       string  `json:"productId"`
+	Quantity        int     `json:"quantity"`
+	Email           string  `json:"email"`
+	OrderDate       string  `json:"orderDate"`
+	Subtotal        float64 `json:"subtotal"`
+	ShippingAddress string  `json:"shippingAddress"`
+	ShippingMethod  string  `json:"shippingMethod"`
+	Type            string  `json:"type"`
 }
