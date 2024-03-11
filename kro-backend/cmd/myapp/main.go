fbd9c702b9a47c926b5a233fa4b92a07216099b3
Kenmu <89627339+Kenmuraki5@users.noreply.github.com>
2024-03-11 08:10:18 +0700

    Update main.go

--- kro-backend/cmd/myapp/main.go
+++ kro-backend/cmd/myapp/main.go
@@ -44,7 +44,7 @@ func main() {
 	router := gin.Default()
 
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://3.87.34.50/"}
+	config.AllowOrigins = []string{"http://*/"}
 	config.AllowMethods = []string{"GET", "POST", "OPTIONS", "PATCH", "DELETE", "PUT"}
 	config.AllowHeaders = []string{"Authorization", "Content-Type"}
 
